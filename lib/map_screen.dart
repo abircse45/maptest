fde76e0333c2fd8ecf3aca9dd67c6f036a76f5ee
abircse45 <abiruodacse@gmail.com>
2022-12-01 00:29:10 +0600

    updated

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geocoder2/geocoder2.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -14,8 +16,12 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   Set<Marker> marker = {};
-
+  // Set<Polyline> _polylines = {};
+  Set<Polyline> _polyLine = {};
+  //double distance = 0.0;
   final Completer<GoogleMapController> _controller = Completer();
+  
+  double distance = 0.0;
 
   final CameraPosition _initialPosition =
       CameraPosition(target: LatLng(23.7808369, 90.4185591), zoom: 14);
@@ -57,20 +63,86 @@ class _MapScreenState extends State<MapScreen> {
     return await Geolocator.getCurrentPosition();
   }
 
+  //
+  // List<LatLng>  latLong = [
+  //   LatLng(23.7470499,90.3655623),
+  //   LatLng(23.869466,90.3849888),
+  // ];
+
   var lat;
   var long;
   var dataAddress;
+
+  Future<Polyline?> getpolyLine(LatLng origin,LatLng to) async {
+    List<LatLng> polyLineCordinate = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        "AIzaSyCN8snQFg1eriDbdHIgHPWxirZKkz2PKyY",
+        PointLatLng(origin.latitude, origin.longitude),
+        PointLatLng(23.869466, 90.3849888),
+        travelMode: TravelMode.driving,
+        avoidFerries: true,
+        avoidHighways: true);
+    result.points.forEach((PointLatLng point) {
+      polyLineCordinate.add(LatLng(point.latitude, point.longitude));
+    });
+    
+    distance = _calcDistance(polyLineCordinate);
+    
+
+    return Polyline(
+        polylineId: PolylineId(result.points.length.toString()),
+        color: Colors.red,
+        width: 4,
+        points: polyLineCordinate);
+  }
+
+  // Future<Polyline> getPoly(LatLng latLng, LatLng to) async {
+  //   // await _orderDetailsController.getData(widget.id!, widget.lat!, widget.long!);
+  //   List<LatLng> polylineCoordinates = [];
+  //   PolylinePoints polylinePoints = PolylinePoints();
+  //   PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+  //       "AIzaSyCN8snQFg1eriDbdHIgHPWxirZKkz2PKyY",
+  //       PointLatLng(
+  //         latLng.latitude,
+  //         latLng.longitude,
+  //       ),
+  //       PointLatLng(23.7227447, 90.4113581),
+  //       travelMode: TravelMode.driving,
+  //       avoidHighways: true,
+  //       avoidFerries: true,
+  //       avoidTolls: true);
+  //
+  //   result.points.forEach((PointLatLng point) {
+  //     polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+  //   });
+  //   // distance = _calcDistance(polylineCoordinates);
+  //   distance = _calcDistance(polylineCoordinates);
+  //
+  //   return Polyline(
+  //     polylineId: PolylineId("polyline_id ${result.points.length}"),
+  //     color: Colors.red,
+  //     points: polylineCoordinates,
+  //     width: 3,
+  //   );
+  //
+  //   //print("DDDDD__${distance}");
+  // }
+
   Future getCurrentLocation() async {
-    var postion = await Geolocator.getCurrentPosition(
+    Position postion = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
 
-
+    setState(() {
+      lat = postion.latitude;
+      long = postion.longitude;
+    });
 
     var address = await Geocoder2.getDataFromCoordinates(
       latitude: postion.latitude,
       longitude: postion.longitude,
-      googleMapApiKey: "AIzaSyD1R65QX1dpR58NlomcImBbFBQKwG9vrYc",
+      googleMapApiKey: "AIzaSyCN8snQFg1eriDbdHIgHPWxirZKkz2PKyY",
     );
     print("Lat__${postion.latitude}");
     print("long__${postion.longitude}");
@@ -88,9 +160,19 @@ class _MapScreenState extends State<MapScreen> {
     dataAddress = address.address;
     final CameraPosition currentPosition = CameraPosition(
         target: LatLng(postion.latitude, postion.longitude), zoom: 14);
+
+    // _drawPolyline(
+    //   LatLng(postion.latitude, postion.longitude),
+    //   LatLng(23.7227447, 90.4113581),
+    // );
+    _drawPolyLine(
+      LatLng(postion.latitude, postion.longitude),
+      LatLng(23.869466, 90.3849888),
+
+    );
+
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
-
   }
 
   @override
@@ -98,7 +180,25 @@ class _MapScreenState extends State<MapScreen> {
     setState(() {
       getCurrentLocation();
     });
-  //  _determinePosition();
+    _determinePosition();
+
+    // for(int i =0; i < latLong.length; i++){
+    //   marker.add(Marker(
+    //     markerId: MarkerId(i.toString()),
+    //     position: latLong[i]
+    //   ));
+    //   setState(() {
+    //
+    //   });
+    //   _polyLine.add(Polyline(
+    //     color: Colors.red,
+    //     width: 2,
+    //     polylineId: PolylineId("1"),
+    //     points:latLong,
+    //   ));
+    //
+    // }
+
     super.initState();
   }
 
@@ -115,7 +215,10 @@ class _MapScreenState extends State<MapScreen> {
         body: GoogleMap(
           markers: marker,
           mapType: MapType.normal,
-          // zoomControlsEnabled: true,
+          polylines: _polyLine,
+          //polylines: _polyLine,
+          //
+          //polylines: _polylines,
           initialCameraPosition: _initialPosition,
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
@@ -124,5 +227,86 @@ class _MapScreenState extends State<MapScreen> {
             });
           },
         ));
+  }
+
+
+  Future  _drawPolyLine(LatLng from, LatLng to)async{
+
+    Polyline? polyline = await getpolyLine(from,to);
+    _polyLine.add(polyline!);
+    setmarker(from);
+    setmarker(to);
+    setState(() {
+
+    });
+
+
+  }
+
+  setmarker(LatLng latLng){
+    Marker _marker = Marker(
+      markerId: MarkerId(latLng.toString()),
+      position: latLng,
+      infoWindow: InfoWindow(
+        title: "${distance.toStringAsFixed(2)} Km"
+      )
+    );
+    marker.add(_marker);
+    setState(() {
+
+    });
+
+  }
+
+  // Future<void> _drawPolyline(LatLng from, LatLng to) async {
+  //   Polyline polyline = await getPoly(from, to);
+  //
+  //   _polylines.add(polyline);
+  //
+  //   _setMarker(from);
+  //   _setMarker(to);
+  //
+  //   setState(() {});
+  // }
+
+  // void _setMarker(LatLng _location) {
+  //   Marker newMarker = Marker(
+  //     markerId: MarkerId(_location.toString()),
+  //     icon: BitmapDescriptor.defaultMarker,
+  //     // icon: _locationIcon,
+  //     position: _location,
+  //     infoWindow: InfoWindow(
+  //       title: "${distance.toStringAsFixed(2)}km",
+  //       // snippet: "${PolylineService().totalDistance.toDouble()}",
+  //     ),
+  //   );
+  //   marker.add(newMarker);
+  //   setState(() {});
+  // }
+  double _calcDistance(List<LatLng> polylineCoordinates) {
+
+    double totalDistance = 0.0;
+
+    for (int i = 0; i < polylineCoordinates.length - 1; i++) {
+      totalDistance += coordinateDistance(
+        polylineCoordinates[i].latitude,
+        polylineCoordinates[i].longitude,
+        polylineCoordinates[i + 1].latitude,
+        polylineCoordinates[i + 1].longitude,
+      );
+    }
+
+    print("distance = ${totalDistance.toStringAsFixed(2)} km");
+
+    return totalDistance;
+  }
+
+  double coordinateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
   }
 }
